c6114f8a3f0829114cbb90bb43f910caac90a6f4
JemberSanchez <sanchezjember13@gmail.com>
2025-07-13 23:30:14 -0500

    Eliminar archivo edad_futura.py

--- edad_futura.py
+++
@@ -1,6 +0,0 @@
-name = input('whats your name?:')
-current_age = int(input('your age now?:'))
-current_year = 2025 
-future_year = int(input('in what year do you want to know your age?:'))
-future_age = current_age + (future_year - current_year)                
-print(f'{name},In the year {future_year} you will be {future_age} years old.')
